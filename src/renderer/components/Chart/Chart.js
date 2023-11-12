478f3850ae6dd7419acbbeee33db6ef2a3f20fd3
Maciej Buszkiewicz <maciek.buszkiewicz@ideeconcept.pl>
2023-11-12 22:01:13 +0100

    feat: performance update

--- src/renderer/components/Chart/Chart.js
+++ src/renderer/components/Chart/Chart.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import XLSX from 'xlsx';
+import { useState } from 'react';
 import { Line } from 'react-chartjs-2';
 import { Chart as ChartJS, registerables } from "chart.js";
 import RangeSlider from 'react-range-slider-input';
@@ -9,77 +8,11 @@ import './Chart.scss'
 
 ChartJS.register(...registerables);
 
-const Chart = ({ selectedFile }) => {
-  const [fileOutput, setFileOutput] = useState([]);
-  let arrayToConvert = [];
-  let transformedData = []
+const Chart = ({ fileData, fileName }) => {  
+  const [range, setRange] = useState({ min: 0, max: fileData.length });  
 
-  const parseExcel = (file) => {
-    let reader = new FileReader();
-
-    reader.onload = function (e) {
-      let data = e.target.result;
-      let workbook = XLSX.read(data, {
-        type: 'binary',
-      });
-
-      workbook.SheetNames.forEach(function (sheetName) {
-        let XL_row_object = XLSX.utils.sheet_to_row_object_array(
-          workbook.Sheets[sheetName],
-        );
-        let json_object = JSON.stringify(XL_row_object);
-        setFileOutput(json_object);
-      });
-    };
-
-    reader.readAsBinaryString(file);
-  };
-
-  parseExcel(selectedFile);
-
-  const convertExcelDate = (excelDate) => {
-    const date = new Date((excelDate - 25569) * 86400 * 1000);
-    return date
-  }
-
-  const convertExcelTime = (excelTime) => {
-    let timeInSeconds;
-
-    if (excelTime > 1) {
-      timeInSeconds = (excelTime - 1) * 86400;
-    } else {
-      timeInSeconds = excelTime * 86400;
-    }
-
-    let time = new Date(timeInSeconds * 1000).toISOString().slice(11, 19)
-    return time
-  }
-
-  try {
-    arrayToConvert = JSON.parse(fileOutput);
-    
-    transformedData = arrayToConvert.map((item) => {
-      const date = convertExcelDate(item.Date)
-      const time = convertExcelTime(item.Time)
-      
-      if ('Ch3' in item) {
-        return { date, time, Ch0: item.Ch0, Ch1: item.Ch1, Ch2: item.Ch2, Ch3: item.Ch3 };
-      }
-      if ('Ch2' in item) {
-        return { date, time, Ch0: item.Ch0, Ch1: item.Ch1, Ch2: item.Ch2 };
-      }
-      if ('Ch1' in item) {
-        return { date, time, Ch0: item.Ch0, Ch1: item.Ch1 };
-      }
-      if ('Ch0' in item) {
-        return { date, time, Ch0: item.Ch0 };
-      }
-    });
-  } catch (error) {
-    console.error('Error parsing JSON data:', error);
-  }
-
-  const [range, setRange] = useState({ min: 0, max: transformedData.length });  
+  console.log('Range: ' + range.max)
+  console.log('filedata: ' + fileData.length)
 
   const handleMinInputChange = (e) => {
     setRange({ ...range, min: e.target.value });
@@ -93,9 +26,9 @@ const Chart = ({ selectedFile }) => {
     setRange({ min: newRange[0] , max: newRange[1] });
   };
 
-  const filteredChartData = transformedData.slice(
+  const filteredChartData = fileData.slice(
     range.min ? parseInt(range.min, 10) : 0,
-    range.max ? parseInt(range.max, 10) : transformedData.length
+    range.max ? parseInt(range.max, 10) : fileData.length
   );
 
   const filteredLabels = filteredChartData.map((item) => {
@@ -109,7 +42,7 @@ const Chart = ({ selectedFile }) => {
     backgroundColor: 'rgba(9, 211, 172, 0.5)',
     borderWidth: 1,
     pointRadius: 2,
-  },
+  }
   const dataset_1 = {
     label: 'Ch1',
     data: filteredChartData.map((item) => item.Ch1),
@@ -117,7 +50,7 @@ const Chart = ({ selectedFile }) => {
     backgroundColor: 'rgba(255, 99, 132, 0.5)',
     borderWidth: 1,
     pointRadius: 2,
-  },
+  }
   const dataset_2 = {
     label: 'Ch2',
     data: filteredChartData.map((item) => item.Ch2),
@@ -125,7 +58,7 @@ const Chart = ({ selectedFile }) => {
     backgroundColor: 'rgba(154, 0, 255, 0.5)',
     borderWidth: 1,
     pointRadius: 2,
-  },
+  }
   const dataset_3 = {
     label: 'Ch3',
     data: filteredChartData.map((item) => item.Ch3),
@@ -133,19 +66,19 @@ const Chart = ({ selectedFile }) => {
     backgroundColor: 'rgba(255, 255, 0, 0.5)',
     borderWidth: 1,
     pointRadius: 2,
-  },
+  }
 
   const datasets = [ dataset_0 ];
   
-  if (transformedData.some((item) => 'Ch1' in item)) {
+  if (fileData.some((item) => 'Ch1' in item)) {
     datasets.push(dataset_1);
   }
 
-  if (transformedData.some((item) => 'Ch2' in item)) {
+  if (fileData.some((item) => 'Ch2' in item)) {
     datasets.push(dataset_2);
   }
 
-  if (transformedData.some((item) => 'Ch3' in item)) {
+  if (fileData.some((item) => 'Ch3' in item)) {
     datasets.push(dataset_3);
   }
   
@@ -153,7 +86,6 @@ const Chart = ({ selectedFile }) => {
     labels: filteredLabels,
     datasets,
   };
-  
 
   const options = {
     responsive: true,
@@ -170,8 +102,8 @@ const Chart = ({ selectedFile }) => {
 
   return (
     <div className='chart'>
-      <h1 className='chart__title'>Selected File: {selectedFile.name}</h1>
-      {transformedData[1] && <p className='chart__date'>Date of research: {transformedData[1].date.toLocaleDateString()}</p>}
+      <h1 className='chart__title'>Selected File: {fileName}</h1>
+      {fileData[1] && <p className='chart__date'>Date of research: {fileData[1].date.toLocaleDateString()}</p>}
       <div className='chart__container'>
         <Line data={data} options={options} width={1000} height={500}/>
       </div>
@@ -191,7 +123,7 @@ const Chart = ({ selectedFile }) => {
           onChange={handleMaxInputChange}
         />
       </div>
-      <RangeSlider min={0} max={transformedData.length} value={[range.min, range.max]} onInput={handleRangeChange} />
+      <RangeSlider min={0} max={fileData.length} value={[range.min, range.max]} onInput={handleRangeChange} />
     </div>
   );
 };
--- src/renderer/pages/Disposable.js
+++ src/renderer/pages/Disposable.js
@@ -1,20 +1,92 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import XLSX from 'xlsx';
 
-import Back from '../components/Back/Back'
-import Input from '../components/Input/Input'
-import Chart from '../components/Chart/Chart'
+import Back from '../components/Back/Back';
+import Input from '../components/Input/Input';
+import Chart from '../components/Chart/Chart';
 
 const DisposablePage = () => {
-  const [ selectedFile, setSelectedFile ] = useState(null)
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [fileData, setFileData] = useState([]);
+  let arrayToConvert = [];
+  let transformedData = [];
+
+  const parseExcel = (file) => {
+    let reader = new FileReader();
+
+    reader.onload = function (e) {
+      let data = e.target.result;
+      let workbook = XLSX.read(data, {
+        type: 'binary',
+      });
+
+      workbook.SheetNames.forEach(function (sheetName) {
+        let XL_row_object = XLSX.utils.sheet_to_row_object_array(
+          workbook.Sheets[sheetName],
+        );
+        let json_object = JSON.stringify(XL_row_object);
+        setFileData(json_object)
+      });
+    };
+
+    reader.readAsBinaryString(file);
+  };
+
+  const convertExcelDate = (excelDate) => {
+    const date = new Date((excelDate - 25569) * 86400 * 1000);
+    return date
+  }
+
+  const convertExcelTime = (excelTime) => {
+    let timeInSeconds;
+
+    if (excelTime > 1) {
+      timeInSeconds = (excelTime - 1) * 86400;
+    } else {
+      timeInSeconds = excelTime * 86400;
+    }
+
+    let time = new Date(timeInSeconds * 1000).toISOString().slice(11, 19)
+    return time
+  }
+
+  useEffect(() => {
+    if (selectedFile) {
+      parseExcel(selectedFile);
+    }
+  }, [selectedFile]);
+
+  try {
+    arrayToConvert = JSON.parse(fileData);
+    
+    transformedData = arrayToConvert.map((item) => {
+      const date = convertExcelDate(item.Date)
+      const time = convertExcelTime(item.Time)
+      
+      if ('Ch3' in item) {
+        return { date, time, Ch0: item.Ch0, Ch1: item.Ch1, Ch2: item.Ch2, Ch3: item.Ch3 };
+      }
+      if ('Ch2' in item) {
+        return { date, time, Ch0: item.Ch0, Ch1: item.Ch1, Ch2: item.Ch2 };
+      }
+      if ('Ch1' in item) {
+        return { date, time, Ch0: item.Ch0, Ch1: item.Ch1 };
+      }
+      if ('Ch0' in item) {
+        return { date, time, Ch0: item.Ch0 };
+      }
+    });
+  } catch (error) {
+    console.error('Error parsing JSON data:', error);
+  }
 
   return (
     <>
       <Back />
       {!selectedFile && <Input setSelectedFile={setSelectedFile} />}
-      {selectedFile && <Chart selectedFile={selectedFile} />}
+      {selectedFile && <Chart fileData={transformedData} fileName={selectedFile.name} />}
     </>
-  )
-}
+  );
+};
 
-export default DisposablePage
+export default DisposablePage;
