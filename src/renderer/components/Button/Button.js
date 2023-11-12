0c06085caaa0af56140657072e1a3e184f02ec9d
Maciej Buszkiewicz <maciek.buszkiewicz@ideeconcept.pl>
2023-11-12 17:32:46 +0100

    feat: changes in getId page

--- src/renderer/components/Button/Button.js
+++ src/renderer/components/Button/Button.js
@@ -3,13 +3,14 @@ import "./Button.scss";
 
 const Button = (props) => {
   const primary = props.primary !== undefined ? props.primary : true;
-  const buttonClasses = primary ? "button button--primary" : "button button--secondary";
+  const buttonPrimary = primary ? "button button--primary" : "button button--secondary";
+  const buttonClasses = props.className ? props.className : ''
 
   const disabled = props.disabled !== undefined ? props.disabled : false;
   const isDisabled = disabled ? " button--disabled" : "";
 
   return (
-    <button className={buttonClasses + isDisabled} onClick={props.onClick} disabled={disabled}>
+    <button className={buttonPrimary + isDisabled + ' ' + buttonClasses} onClick={props.onClick} disabled={disabled}>
       {props.children}
     </button>
   );
